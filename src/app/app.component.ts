eef4ca72c6f9e962f845950b333968bc22063b3d
CHEN Qing <chenqing1213@gmail.com>
2017-05-10 10:50:34 +0800

    makes ng-lottie@0.2.0 works with 'ng build -prod'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,53 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styles: [`
+    #player{
+       text-align: center;
+    }
+
+    .range-container{
+      display: flex;
+      justify-content: center;
+    }
+
+    .range-container input{
+      width:200px;
+      margin-bottom: 10px;
+    }
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+    lottieConfig: Object;
+    anim: any;
+    animationSpeed: number = 1;
+ 
+    constructor() {
+        this.lottieConfig = {
+            path: '../assets/pinjump.json',
+            autoplay: true,
+            loop: true
+        };
+    }
+ 
+    handleAnimation(anim: any) {
+        this.anim = anim;
+    }
+ 
+    stop() {
+        this.anim.stop();
+    }
+ 
+    play() {
+        this.anim.play();
+    }
+ 
+    pause() {
+        this.anim.pause();
+    }
+ 
+    setSpeed(speed: number) {
+        this.animationSpeed = speed;
+        this.anim.setSpeed(speed);
+    }
 }
